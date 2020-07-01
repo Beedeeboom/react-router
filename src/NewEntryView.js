0c415a075adc62c869228ee930210546acee9dad
Beedeeboom <stephanie.duffieux@gmail.com>
2020-07-01 14:32:46 +1000

    added category props

--- src/NewEntryView.js
+++ src/NewEntryView.js
@@ -2,10 +2,11 @@ import React from 'react'
 
 export default function NewEntryView() {
     const { cat_id } = props.match.params
+    const { categories } = props
     return (
         <div>
             <h1>New Entry</h1>
-            <h5>Category: { cat_id }</h5>
+            <h4>Category: {categories[cat_id]}</h4>
         </div>
     )
 }
