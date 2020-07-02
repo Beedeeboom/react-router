b43e3d26a62773365582970278fd3f65a52e7269
Beedeeboom <stephanie.duffieux@gmail.com>
2020-07-02 12:18:44 +1000

    adding new entry form

--- src/App.js
+++ src/App.js
@@ -4,19 +4,25 @@ import HomeView from './HomeView'
 import CategorySelectionView from './CategorySelectionView'
 import NewEntryView from './NewEntryView'
 
-const App = () => { 
-  const categories = ['development', 'movies', 'philosophy', 'other']
+const App = () => {
+  const [categories, setCategories] = useState(['development', 'movies', 'philosophy', 'other'])
+  const [entries, setEntries] = useState([])
+
+  function addEntry(entry) {
+    setEntries([...entries, entry])
+  }
+
   return (
     <div>
       <BrowserRouter>
         <nav>
-            <Link to="/">Home</Link>
-            <Link to="/category">Category</Link>
-            <Link to="/entry">New Entry</Link>
+          <Link to="/">Home</Link>
+          <Link to="/category">Category</Link>
+          <Link to="/entry">New Entry</Link>
         </nav>
         <Route exact path="/" component={HomeView} />
         <Route exact path="/category" render={props => <CategorySelectionView {...props} categories={categories} />} />
-        <Route exact path="/newEntry/:cat_id" render={props => <NewEntryView {...props} categories={categories}/>} />
+        <Route exact path="/entry/new/:cat_id" render={props => <NewEntryView {...props} addEntry={addEntry} categories={categories} />} />
       </BrowserRouter>
     </div>
   )
--- src/NewEntryView.js
+++ src/NewEntryView.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function NewEntryView() {
+export default function NewEntryView(props) {
     const { cat_id } = props.match.params
-    const { categories } = props
+    const { categories, addEntry } = props
+    const [entry, setEntry] = useState("")
+
     return (
         <div>
             <h1>New Entry</h1>
             <h4>Category: {categories[cat_id]}</h4>
+            <form onSubmit={e => { e.preventDefault(); addEntry({ entry, category: categories[cat_id] }) }}>
+                <div>
+                    <textarea value={entry} onChange={e => setEntry(e.target.value)}></textarea>
+                </div>
+                <button>Create New Entry</button>
+            </form>
         </div>
     )
 }
